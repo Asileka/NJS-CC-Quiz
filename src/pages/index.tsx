5b3c7f9af1dd65aded59fc67e24460cee7b33d18
Asileka <asilkatya@gmail.com>
2023-04-27 09:04:10 +0100

    backup commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,38 @@
 import Head from "next/head";
+import axios from "axios";
 
-export default function Home() {
-  interface quiz {
-    id: number;
-    name: string;
-    question1: string;
-    question2: string;
-    question3: string;
-  }
+interface quiz {
+  id: number;
+  name: string;
+  question1: string;
+  question2: string;
+  question3: string;
+}
+
+interface PageProps {
+  quizzes: quiz[];
+}
+
+export default function Home(props: PageProps) {
   return (
     <>
       <Head>
-        <title>Ekaterina Tech Test</title>
+        <title>Ekaterina Tech Test </title>
       </Head>
       <main>
-        <h1 className="text-6xl text-black font-semibold">Quizzes List</h1>
+        <h1 className="text-6xl text-black font-semibold">
+          Quizzes List {props.quizzes[0].name}
+        </h1>
       </main>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const response = await axios.get("http://localhost:3000/api/quiz/getQuizzes");
+  return {
+    props: {
+      quizzes: response.data,
+    },
+  };
 }
